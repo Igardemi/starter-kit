eb07848454c4730f8ba715e1886521751387405c
Ivan Garcia <igardemi@hotmail.es>
2024-03-05 16:36:32 +0100

    quitando livekit

--- src/pages/livekit/roomComponent.tsx
+++ src/pages/livekit/roomComponent.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import '@livekit/components-styles'
-import { ControlBar, LiveKitRoom, RoomAudioRenderer } from '@livekit/components-react'
 
 // Define las props que espera recibir el componente
 interface RoomComponentProps {
@@ -17,10 +15,9 @@ const RoomComponent: React.FC<RoomComponentProps> = ({ token }) => {
   }
 
   return (
-    <LiveKitRoom video={true} audio={true} token={token} serverUrl={'wss://futuraspaceserver12.link'} connect={true}>
-      <RoomAudioRenderer />
-      <ControlBar />
-    </LiveKitRoom>
+    <div className='d-flex justify-content-center align-items-center' style={{ backgroundColor: 'black' }}>
+      <h2 className='text-white'>{token}</h2>
+    </div>
   )
 }
 
