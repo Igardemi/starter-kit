d9a18f9f1e792378282ee8e309ee0f58477edea1
Ivan Garcia <igardemi@hotmail.es>
2024-03-05 21:46:52 +0100

    string isvalirdrc

--- src/pages/games/UnityCanvas.tsx
+++ src/pages/games/UnityCanvas.tsx
@@ -21,6 +21,22 @@ const UnityCanvas: React.FC<UnityCanvasProps> = ({ src }) => {
   const { unityProvider, loadingProgression, isLoaded, unload, requestFullscreen } = useUnityContext(config)
   const router = useRouter()
 
+  //comprobar si es valido el src
+  const isValidSrc = (src: string) => {
+    if (!src) return false
+
+    const pattern = new RegExp(
+      '^(https?:\\/\\/)?' +
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$',
+      'i'
+    )
+    return !!pattern.test(src)
+  }
+
   useEffect(() => {
     const updateConfig = async () => {
       if (isConfigured) {
@@ -28,7 +44,7 @@ const UnityCanvas: React.FC<UnityCanvasProps> = ({ src }) => {
         await handleClickBack()
         setIsConfigured(false)
       }
-      if (!isConfigured) {
+      if (!isConfigured && isValidSrc(src)) {
         setConfig({
           loaderUrl: `${src}.loader.js`,
           dataUrl: `${src}.data`,
